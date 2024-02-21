4d765924ec2e7aa803ed3a21093834b6c80d03b3
Kuk Hoon Ryou <coocoon09@gmail.com>
2024-02-21 12:54:31 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+var customerName = 'bob'
+const leastFavoriteCustomer = 1
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    //return customerName.toUpperCase()     
+}
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    //return bestCustomer
+    //return bestCustomer
+}    
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = '5'
+}
+
 // Write your solution in this file!
