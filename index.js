8a850979411a57e25ae0c55e7b263d7e651d4292
Kuk Hoon Ryou <coocoon09@gmail.com>
2024-02-22 12:24:07 -0500

    teacher's solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var customerName = 'bob'
+/*var customerName = 'bob'
 const leastFavoriteCustomer = 1
 
 function upperCaseCustomerName() {
@@ -19,5 +19,27 @@ function overwriteBestCustomer() {
 function changeLeastFavoriteCustomer() {
     leastFavoriteCustomer = '5'
 }
+*/
 
 // Write your solution in this file!
+
+
+//Teacher's Solution
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'mary'
+var upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase();
+}
+
+var setBestCustomer = () => {
+    bestCustomer = 'not bob'
+}
+
+var overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob'
+}
+
+var changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'tim'
+}
